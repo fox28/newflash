946fa202d3bb489d13f4ded0f9056f56cd912f4a
marmot <279099862@qq.com>
2017-09-11 14:08:40 +0800

    修改反馈信息界面：xml文件的背景颜色改为红色；完成返回按钮的功能。

--- app/src/main/java/com/isuhuo/newsflash/feedback/FeedbackActivity.java
+++ app/src/main/java/com/isuhuo/newsflash/feedback/FeedbackActivity.java
@@ -3,17 +3,49 @@ package com.isuhuo.newsflash.feedback;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.isuhuo.newsflash.R;
+import com.isuhuo.newsflash.util.MFGT;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
  * Created by 鱼 on 2017/8/27.
  */
-public class FeedbackActivity extends AppCompatActivity{
+public class FeedbackActivity extends AppCompatActivity {
+    @BindView(R.id.fankui_fanhui)
+    LinearLayout mBackArea;
+
+    Unbinder bind;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        bind = ButterKnife.bind(this);
+        initListener();
+    }
+
+    private void initListener() {
+        mBackArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MFGT.finish(FeedbackActivity.this);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bind.unbind();
+        this.finish();
+
     }
 }
