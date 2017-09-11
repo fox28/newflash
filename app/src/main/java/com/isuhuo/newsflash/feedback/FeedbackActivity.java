97ebd06c045d65d14b673f16db299068da42b481
marmot <279099862@qq.com>
2017-09-11 15:16:27 +0800

    删除多余的黄油刀依赖；反馈信息界面，提交信息的非空验证功能完成； 提交信息的API需要uid、必须先整理注册部分代码，未完成部分TODO标注

--- app/src/main/java/com/isuhuo/newsflash/feedback/FeedbackActivity.java
+++ app/src/main/java/com/isuhuo/newsflash/feedback/FeedbackActivity.java
@@ -3,14 +3,18 @@ package com.isuhuo.newsflash.feedback;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.isuhuo.newsflash.R;
 import com.isuhuo.newsflash.util.MFGT;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 
@@ -20,8 +24,11 @@ import butterknife.Unbinder;
 public class FeedbackActivity extends AppCompatActivity {
     @BindView(R.id.fankui_fanhui)
     LinearLayout mBackArea;
+    @BindView(R.id.fankui_edit)
+    EditText mEtContent;
 
     Unbinder bind;
+    private String feedbackContent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,5 +54,15 @@ public class FeedbackActivity extends AppCompatActivity {
         bind.unbind();
         this.finish();
 
+    }
+
+    @OnClick(R.id.fankui_bt)
+    public void onCommitClicked() {
+        feedbackContent = mEtContent.getText().toString().trim();
+        if (TextUtils.isEmpty(feedbackContent)) {
+            Toast.makeText(this, "提交信息不能为空", Toast.LENGTH_SHORT).show();
+        } else {
+            // TODO 请求服务器，feedback接口
+        }
     }
 }
