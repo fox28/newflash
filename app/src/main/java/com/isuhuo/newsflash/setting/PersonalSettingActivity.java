148bbf9bd1484082d5922387524cf0631cb069ea
marmot <279099862@qq.com>
2017-09-11 22:05:25 +0800

    PersonalSettingActivity布局文件显示已登录的用户头像和昵称

--- app/src/main/java/com/isuhuo/newsflash/setting/PersonalSettingActivity.java
+++ app/src/main/java/com/isuhuo/newsflash/setting/PersonalSettingActivity.java
@@ -1,16 +1,62 @@
 package com.isuhuo.newsflash.setting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.isuhuo.newsflash.R;
+import com.isuhuo.newsflash.base.MyAppLocation;
+import com.isuhuo.newsflash.ui.activity.MainActivity;
+import com.isuhuo.newsflash.util.SpUtils;
+import com.isuhuo.newsflash.util.UserBeen;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class PersonalSettingActivity extends AppCompatActivity {
+    private static final String TAG = "PersonalSettingActivity";
+    private UserBeen user = new UserBeen();
+
+    @BindView(R.id.iv_user_profile_avatar)
+    ImageView mIvUserProfileAvatar;
+    @BindView(R.id.tv_user_profile_nick)
+    TextView mTvUserProfileNick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_setting);
+        ButterKnife.bind(this);
+
+        user = MyAppLocation.app.getUser();
+        initView();
+    }
+
+    private void initView() {
+        if (user != null) {
+            Glide.with(PersonalSettingActivity.this)
+                    .load(user.getUser_head_img())
+                    .into(mIvUserProfileAvatar);
+            // user.getName() == SpUtils.getUser(this, "name", null)
+            mTvUserProfileNick.setText(user.getName());
+        }
+    }
+
+    @OnClick({R.id.backArea, R.id.layout_user_profile_avatar, R.id.layout_user_profile_nickname})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.backArea:
+                break;
+            case R.id.layout_user_profile_avatar:
+                break;
+            case R.id.layout_user_profile_nickname:
+                break;
+        }
     }
 }
